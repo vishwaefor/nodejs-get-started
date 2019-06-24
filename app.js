7157ab8e67f3026ed85f026f3988dfc8800e2413
Vishwa Jayarathna <vishwa@codegen.net>
2019-06-24 20:31:02 +0530

    cors headers

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser')
 
 const studentRoutes = require('./api/routes/students')
 
-app.use(morgan('dev'))
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.use((req, res, next) => {
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Request-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    }
+
+    next();
+});
+
+
 app.use('/students', studentRoutes);
 
 
