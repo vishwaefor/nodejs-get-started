c7ce6294188a07bf314d1b94462a352c663a026f
Vishwa Jayarathna <vishwa@codegen.net>
2019-06-24 17:26:14 +0530

    error handling

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 
 const studentRoutes = require('./api/routes/students')
 
-app.use('/students',studentRoutes);
+app.use(morgan('dev'))
+app.use('/students', studentRoutes);
+
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+})
+
+
 
 module.exports = app;
